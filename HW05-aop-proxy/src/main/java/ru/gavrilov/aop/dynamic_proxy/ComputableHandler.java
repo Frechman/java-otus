eb2eeddb5d8435ef7995122a9867e7e09a1bc87d
sj <serj.g13@yandex.ru>
2020-05-11 01:05:30 +0300

    HW05: fixed ComputableHandler

--- HW05-aop-proxy/src/main/java/ru/gavrilov/aop/dynamic_proxy/ComputableHandler.java
+++ HW05-aop-proxy/src/main/java/ru/gavrilov/aop/dynamic_proxy/ComputableHandler.java
@@ -9,23 +9,22 @@ import java.util.stream.Collectors;
 public class ComputableHandler implements InvocationHandler {
 
     private final Computable computable;
-    private final Set<String> methodNamesForLogging;
+    private final Set<Method> methodNamesForLogging;
 
     public ComputableHandler(Computable computable) {
         this.computable = computable;
         this.methodNamesForLogging = getMethodNamesForLogging();
     }
 
-    private Set<String> getMethodNamesForLogging() {
+    private Set<Method> getMethodNamesForLogging() {
         return Arrays.stream(computable.getClass().getDeclaredMethods())
                 .filter(m -> m.isAnnotationPresent(Log.class))
-                .map(Method::getName)
                 .collect(Collectors.toSet());
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (methodNamesForLogging.contains(method.getName())) {
+        if (methodNamesForLogging.contains(method)) {
             System.out.println(String.format("executed method: %s, param: %s", method.getName(), args[0]));
         }
         return method.invoke(computable, args);
